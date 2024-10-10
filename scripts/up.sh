01dc92de46befe4c1bb93b22300c0e3f57622ec9
anhhung04 <dev.hah4@gmail.com>
2024-10-10 14:14:15 +0700

    fix find port

--- scripts/up.sh
+++ scripts/up.sh
@@ -46,21 +46,27 @@ if [ -z "$CHECKERS" ]; then
   CHECKER_LIST=$SERVICE_LIST
 fi
 
+info() {
+  echo -e "\033[1;32m[INFO]\033[0m $1"
+}
+
+error() {
+  echo -e "\033[1;31m[ERROR]\033[0m $1"
+}
+
 # Check if the length of the checker list is equal to the length of the service list
 if [ "$(echo $SERVICE_LIST | wc -w)" != "$(echo $CHECKER_LIST | wc -w)" ]; then
-  echo "Error: The number of checkers must be equal to the number of services."
+  error "The number of checkers must be equal to the number of services."
   exit 1
 fi
 
+info "Creating empty teamdata directory..."
 # Create empty teamdata directory
 rm -rf ./.docker/api/teamdata
 mkdir ./.docker/api/teamdata
 echo "Team data download links (distribute one link to each team):" >./teamdata.txt
 
-info() {
-  echo -e "\033[1;32m[INFO]\033[0m $1"
-}
-
+info "Generating team tokens..."
 # Generate team tokens
 TEAM_TOKENS=""
 for TEAM_ID in $(seq 1 $TEAM_COUNT); do
@@ -77,15 +83,15 @@ done
 # Strip leading comma
 TEAM_TOKENS="${TEAM_TOKENS:1}"
 
-echo "Starting range services..."
+info "Starting range services..."
 API_KEY=$API_KEY TEAM_COUNT=$TEAM_COUNT PEERS=$VPN_COUNT FLAG_LIFETIME=$FLAG_LIFETIME TICK_SECONDS=$TICK_SECONDS START_TIME=$START_TIME END_TIME=$END_TIME START_TIME_PATH=$START_TIME_PATH SERVERURL=$SERVER_URL FRONTEND_PORT=$FRONTEND_PORT API_PORT=$API_PORT VPN_PORT=$VPN_PORT VPN_DNS=$VPN_DNS TEAM_TOKENS=$TEAM_TOKENS SERVICES=$SERVICES CHECKERS=$CHECKERS docker compose up -d --force-recreate >>./debug.log 2>&1
 
-echo "Waiting for VPN to start..."
+info "Waiting for VPN to start..."
 sleep 5
 
 # If capture pcaps is true, start tcpdump on the vpn interface for the network 10.101.0.0/16 writing to /pcaps/range_$START_TIME.pcap
 if [ "$CAPTURE_PCAPS" = true ]; then
-  echo "Starting tcpdump on vpn interface..."
+  info "Starting tcpdump on vpn interface..."
   mkdir -p ./pcaps/$START_TIME_PATH
   docker exec -d vpn sh -c 'apk update && apk add tcpdump && tcpdump -i any -G $TICK_SECONDS -w /pcaps/$START_TIME_PATH/range_$START_TIME_%Y-%m-%dT%H-%M-%SZ.pcap -s 0 -S net 10.101.0.0/15'
 fi
@@ -94,23 +100,34 @@ find_ports() {
   local compose_file=$(find "$1" -name 'docker-compose.yaml' -o -name 'docker-compose.yml' | head -n 1)
 
   if [ ! -f "$compose_file" ]; then
-    echo "Error: Docker Compose file '$compose_file' not found."
-    return 1
+    echo "Error: Docker Compose file not found in directory '$1'." >&2
+    return ""
   fi
-
-  echo "Published ports in $compose_file:"
-  grep -E '^\s*-\s*"?[0-9]+:[0-9]+"?' "$compose_file" |
-    sed -E 's/.*"?([0-9]+):([0-9]+)"?.*/\1 -> \2/' |
+  local ports=$(grep -E '^\s*-\s*(["'\'']?)([0-9.:]+:)?[0-9]+:[0-9]+\1' "$compose_file" |
+    sed -E 's/^\s*-\s*(["'\'']?)(([0-9.]+:)?([0-9]+):[0-9]+)\1.*/\4/' |
     sort -n |
     uniq |
-    while read -r line; do
-      echo "  $line"
-    done
+    tr '\n' ' ' |
+    sed 's/ $//')
+
+  echo "$ports"
+}
+
+replace_placeholder() {
+  local file="$1"
+  local placeholder="$2"
+  local replacement="$3"
+
+  placeholder=$(printf '%s\n' "$placeholder" | sed -e 's/[]\/$*.^[]/\\&/g')
+  replacement=$(printf '%s\n' "$replacement" | sed -e ':a;N;$!ba;s/\\n/\n/g')
+  replacement=$(printf '%s\n' "$replacement" | sed -e 's/\//\\\//g')
+
+  sed -i ':a;N;$!ba;s/'"$placeholder"'/'"$replacement"'/g' "$file"
 }
 
 # Loop from 1 to $TEAM_COUNT - 1
 for TEAM_ID in $(seq 1 $TEAM_COUNT); do
-  echo "Starting team $TEAM_ID..."
+  info "Starting team $TEAM_ID..."
   TEAM_TOKEN="$(echo $TEAM_TOKENS | cut -d',' -f$TEAM_ID)"
   # Copy vpn files
   for VPN_ID in $(seq 1 $VPN_PER_TEAM); do
@@ -123,19 +140,19 @@ for TEAM_ID in $(seq 1 $TEAM_COUNT); do
   IPTABLE_RULES=""
   for SERVICE_NAME in $SERVICE_LIST; do
     SERVICE_INDEX=$(expr $SERVICE_INDEX + 1)
-    if [ "$(echo $SERVICES | cut -d, -f$SERVICE_INDEX)" == "$(echo $SERVICES, | cut -d, -f$(expr $SERVICE_INDEX + 1))" ]; then
+    if [ "$(echo $SERVICES | cut -d, -f$(expr $SERVICE_INDEX - 1))" == "$(echo $SERVICES, | cut -d, -f$(expr $SERVICE_INDEX))" ]; then
       continue
     fi
+    info "Starting service $SERVICE_NAME for team $TEAM_ID..."
     SERVICE_ID=$(expr $SERVICE_ID + 1)
     dir="./services/$SERVICE/"
     # If the file is a directory
     if [ -d "$dir" ]; then
       # Start docker compose in the /services directory with a volume mount of the directory
       # Generate a random root password
-      local exposed_ports=$(find_ports "./services/$SERVICE_NAME")
+      exposed_ports=$(find_ports "./services/$SERVICE_NAME")
       for port in $exposed_ports; do
-        IPTABLE_RULES="$IPTABLE_RULES\niptables -t nat -A PREROUTING -p tcp --dport $port -j DNAT --to-destination 10.100.$TEAM_ID.$SERVICE_ID:$port"
-        IPTABLE_RULES="$IPTABLE_RULES\niptables -t nat -A POSTROUTING -p tcp -d 10.100.$TEAM_ID.$SERVICE_ID --dport $port -j MASQUERADE"
+        IPTABLE_RULES=$IPTABLE_RULES"iptables -t nat -A PREROUTING -p tcp --dport $port -j DNAT --to-destination 10.100.$TEAM_ID.$SERVICE_ID:$port\n"
       done
       ROOT_PASSWORD="$(openssl rand -hex 16)"
       HOSTNAME=$(echo "team$TEAM_ID-$SERVICE_NAME" | tr '[:upper:]' '[:lower:]')
@@ -145,15 +162,17 @@ for TEAM_ID in $(seq 1 $TEAM_COUNT); do
       IP=$IP HOSTNAME=$HOSTNAME TEAM_ID=$TEAM_ID SERVICE_ID=$SERVICE_ID SERVICE_NAME=$SERVICE_NAME ROOT_PASSWORD=$ROOT_PASSWORD CPU_LIMIT=$CPU_LIMIT MEM_LIMIT=$MEM_LIMIT docker compose -f ./services/docker-compose.yaml --project-name $HOSTNAME up -d >>./debug.log 2>&1
     fi
   done
+  IPTABLE_RULES="$IPTABLE_RULES\niptables -t nat -A POSTROUTING -p tcp -d 10.100.$TEAM_ID.0/24 -j MASQUERADE"
   info "Starting proxy for $TEAM_ID..."
   # Start proxy for team
-  info "IPTABLE_RULES: $IPTABLE_RULES"
+  info "IPTABLE_RULES:\n$IPTABLE_RULES"
   cp ./.docker/proxy/entrypoint.sh.bak ./.docker/proxy/entrypoint.sh
-  sed -i "s/{IPTABLE_RULES}/$IPTABLE_RULES/g" ./.docker/proxy/entrypoint.sh
+  replace_placeholder ./.docker/proxy/entrypoint.sh "IPTABLE_RULES" "$IPTABLE_RULES"
   IP=$(echo "10.100.$TEAM_ID.1" | tr '[:upper:]' '[:lower:]')
   HOSTNAME=$(echo "team$TEAM_ID-proxy" | tr '[:upper:]' '[:lower:]')
+  echo "$IP ($HOSTNAME) - root : root" >>./.docker/api/teamdata/$TEAM_TOKEN/creds.txt
   IP=$IP HOSTNAME=$HOSTNAME TEAM_ID=$TEAM_ID docker compose -f ./.docker/proxy/docker-compose.yml --project-name $HOSTNAME-proxy up -d >>./debug.log 2>&1
-  rm -f ./.docker/proxy/entrypoint.sh.bak
+  rm -f ./.docker/proxy/entrypoint.sh
   # Zip teamdata directory
   pushd ./.docker/api/teamdata >/dev/null
   zip -r $TEAM_TOKEN.zip $TEAM_TOKEN >/dev/null
